d597b59667c55e178fd1c8d95ebea556458a19c3
Jason Nguyen <jasonnguyen141091@gmail.com>
2021-07-31 21:30:15 +0700

    clean code

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -31,7 +31,9 @@ const Page = ({
         transition: 'top 0.2s linear',
       }}>
         <div className="logo" style={{ position: 'fixed', left: '60px' }}>
-          <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="The Movie Database (TMDb)" width="154" height="20"/>
+          <a href="/">
+            <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="The Movie Database (TMDb)" width="154" height="20"/>
+          </a>
         </div>
       </Header>
       {siderLeft && <Sider
